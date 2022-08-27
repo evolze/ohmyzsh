386463cdfb2f1bafa019271119158a3045c686f0
Evolze <58073471+evolze@users.noreply.github.com>
2022-08-27 04:21:11 +0000

    Update uninstall.sh

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -25,11 +25,11 @@ else
   echo "No original zsh config found"
 fi
 
-if hash chsh >/dev/null 2>&1 && [ -f ~/.shell.pre-oh-my-zsh ]; then
-  old_shell=$(cat ~/.shell.pre-oh-my-zsh)
+if hash chsh >/dev/null 2>&1 && [ -f ~/.config/.zsh/.shell.pre-oh-my-zsh ]; then
+  old_shell=$(cat ~/.config/.zsh/.shell.pre-oh-my-zsh)
   echo "Switching your shell back to '$old_shell':"
   if chsh -s "$old_shell"; then
-    rm -f ~/.shell.pre-oh-my-zsh
+    rm -f ~/.config/.zsh/.shell.pre-oh-my-zsh
   else
     echo "Could not change default shell. Change it manually by running chsh"
     echo "or editing the /etc/passwd file."
