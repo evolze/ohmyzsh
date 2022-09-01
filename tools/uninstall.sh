aef813090b4b05debcd95ca81190a62f64ee2b03
Evolze <58073471+evolze@users.noreply.github.com>
2022-09-01 22:51:34 +0000

    Updated uninstall.sh
    Changed .config/.zsh to .config/zsh to match the formatting of other directories within .config.

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -5,31 +5,31 @@ if [ "$confirmation" != y ] && [ "$confirmation" != Y ]; then
 fi
 
 echo "Removing ~/.oh-my-zsh"
-if [ -d ~/.config/.zsh/.oh-my-zsh ]; then
-  rm -rf ~/.config/.zsh/.oh-my-zsh
+if [ -d ~/.config/zsh/.oh-my-zsh ]; then
+  rm -rf ~/.config/zsh/.oh-my-zsh
 fi
 
-if [ -e ~/.config/.zsh/.zshrc ]; then
-  ZSHRC_SAVE=~/.config/.zsh/.zshrc.omz-uninstalled-$(date +%Y-%m-%d_%H-%M-%S)
-  echo "Found ~/.config/.zsh/.zshrc -- Renaming to ${ZSHRC_SAVE}"
-  mv ~/.config/.zsh/.zshrc "${ZSHRC_SAVE}"
+if [ -e ~/.config/zsh/.zshrc ]; then
+  ZSHRC_SAVE=~/.config/zsh/.zshrc.omz-uninstalled-$(date +%Y-%m-%d_%H-%M-%S)
+  echo "Found ~/.config/zsh/.zshrc -- Renaming to ${ZSHRC_SAVE}"
+  mv ~/.config/zsh/.zshrc "${ZSHRC_SAVE}"
 fi
 
 echo "Looking for original zsh config..."
-ZSHRC_ORIG=~/.config/.zsh/.zshrc.pre-oh-my-zsh
+ZSHRC_ORIG=~/.config/zsh/.zshrc.pre-oh-my-zsh
 if [ -e "$ZSHRC_ORIG" ]; then
-  echo "Found $ZSHRC_ORIG -- Restoring to ~/.config/.zsh/.zshrc"
-  mv "$ZSHRC_ORIG" ~/.config/.zsh/.zshrc
+  echo "Found $ZSHRC_ORIG -- Restoring to ~/.config/zsh/.zshrc"
+  mv "$ZSHRC_ORIG" ~/.config/zsh/.zshrc
   echo "Your original zsh config was restored."
 else
   echo "No original zsh config found"
 fi
 
-if hash chsh >/dev/null 2>&1 && [ -f ~/.config/.zsh/.shell.pre-oh-my-zsh ]; then
-  old_shell=$(cat ~/.config/.zsh/.shell.pre-oh-my-zsh)
+if hash chsh >/dev/null 2>&1 && [ -f ~/.config/zsh/.shell.pre-oh-my-zsh ]; then
+  old_shell=$(cat ~/.config/zsh/.shell.pre-oh-my-zsh)
   echo "Switching your shell back to '$old_shell':"
   if chsh -s "$old_shell"; then
-    rm -f ~/.config/.zsh/.shell.pre-oh-my-zsh
+    rm -f ~/.config/zsh/.shell.pre-oh-my-zsh
   else
     echo "Could not change default shell. Change it manually by running chsh"
     echo "or editing the /etc/passwd file."
